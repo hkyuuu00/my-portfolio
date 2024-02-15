abeeffc3aa912d4b735e26d2df3040780e5743e9
hkyuuu00 <lhk3121@gmail.com>
2024-02-15 13:51:06 +0900

    배포 설정

---
+++ src/Component/Article.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Article.css'
+
+export default function Article() {
+  return (
+    <div className='article'>
+        <p className='name'>NAME</p>
+        <p className='phone'>PHONE</p>
+        <p className='adress'>ADRESS</p>
+        <p className='email'>EMAIL</p>
+    </div>
+  )
+}
