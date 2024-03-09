37f41274bb6baca02240d1bebf0abb9e25d4858d
hkyuuu00 <lhk3121@gmail.com>
2024-03-09 16:22:42 +0900

    메뉴바 만들기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Header from './Component/Header';
-import Article from './Component/Article';
+import Home from './Component/Home';
+import Menu from './Component/Menu';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <Article />
+    <div className='app' style={{display: 'flex'}}>
+      <BrowserRouter>
+        {/*<Route path='/' element={<Home />}/>*/}
+        <Menu />
+        <Home />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Component/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Menu.scss';
+import {Link} from 'react-router-dom';
+
+export default function Menu() {
+  const menuList = ['Home','About','Skills','Project','Experience','Education','Blog']
+  return (
+    <div className='menu'>
+      <ul className='ul'>
+        {menuList.map((menu) => 
+          <li key={menu} className='link'>
+            <Link to={`${menu}`}>{menu}</Link>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
